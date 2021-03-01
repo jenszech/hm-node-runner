74d94b258751ddd08e81377dffaeb0d93cb3351d
Zech, Jens <j.zech@thalia.de>
2021-03-01 14:46:01 +0100

    debug

--- lib/src/healthcheck/Status.d.ts
+++ lib/src/healthcheck/Status.d.ts
@@ -1,7 +1,7 @@
 import { DeviceStatistic, VariableStatistic } from 'homematic-js-xmlapi';
 export declare class Status {
-  serviceStart: Date;
-  deviceStats: DeviceStatistic;
-  variableStats: VariableStatistic;
-  getJson(): object;
+    serviceStart: Date;
+    deviceStats: DeviceStatistic;
+    variableStats: VariableStatistic;
+    getJson(): object;
 }
